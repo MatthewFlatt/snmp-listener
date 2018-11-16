d60aac71cb8d5df3040a768c119f9cd535f1c40f
MatthewFlatt <matthew.flatt@red-gate.com>
2018-11-16 13:08:19 +0000

    Add service install PS

---
+++ InstallAlertActionerService.ps1
@@ -0,0 +1,3 @@
+﻿$location = Get-Location
+$location = Join-Path $location "AlertActioner.exe"
+New-Service -Name "AlertActioner" -BinaryPathName $location -DisplayName "Alert actioner for SQL Monitor" -Description "Alert actioner for SQL Monitor" -StartupType Automatic -Credential "red-gate\sqlresponsetest"
