8a80c880da8e6293e306b6df6937b93ea68f8feb
Matthew Flatt <matthew.flatt@red-gate.com>
2018-11-23 10:07:36 +0000

    stage sql poller

---
+++ AlertActioner/RepositoryPoller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Data.SqlClient;
+
+namespace AlertActioner
+{
+    class RepositoryPoller
+    {
+        private static readonly ILog Logger = LogManager.GetLogger("Repository poller");
+        private string _connectionString;
+        private long _lastPollTime;
+
+        public RepositoryPoller(string connectionString)
+        {
+            _connectionString = connectionString;
+            _lastPollTime = DateTime.UtcNow.Ticks;
+        }
+
+        public List<AlertData> Poll()
+        {
+            var alerts = new List<AlertData>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                var query = $"";
+            }
+            return alerts;
+        }
+    }
+}
